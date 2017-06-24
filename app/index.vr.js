59eab11192d1258dc478cee60cffe41d1f96e577
ctsygiel <ctsygiel@gmail.com>
2017-06-23 21:15:09 -1000

    react vr initialized

--- app/index.vr.js
+++ app/index.vr.js
@@ -1,2 +1,12 @@
 import React, { Component } from 'react';
 import { AppRegistry, asset, Pano, Text, View } from 'react-vr';
+
+export default class theater extends Component {
+  render() {
+    return (
+      <View>
+        
+      </View>
+    )
+  }
+}
