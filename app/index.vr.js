2ea7856ef680bef9da489c51e545afff50dd4c4a
ctsygiel <ctsygiel@gmail.com>
2017-06-24 23:40:37 -1000

    accessed vimeo API with GET method of all videos in a particular channel

--- app/index.vr.js
+++ app/index.vr.js
@@ -3,6 +3,19 @@ import { AppRegistry, asset, Pano, Text, View } from 'react-vr';
 import Screen from './components/Screen.js'
 
 export default class Theater extends Component {
+  componentDidMount(){
+    fetch('https://api.vimeo.com/channels/staffpicks/videos', {
+      method: 'GET',
+      headers: {
+        'Host': 'api.vimeo.com',
+        'Content-Type': 'application/vnd.vimeo.video+json',
+        'Authorization': 'bearer 779475c5d1b10ec3aaaae4eed892088a'
+      }
+    })
+    .then(response => response.json())
+    .then(json => console.log('json', json))
+  }
+
   render() {
     return (
       <View>
